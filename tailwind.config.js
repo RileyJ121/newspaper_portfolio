7752f9d951fea9a27defe2ca2c02017c63740419
RileyJ121 <riley.c.json@gmail.com>
2024-10-10 11:24:13 -0500

    Set up tailwind and created body div

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: { 'pixel': '5px 5px 3px 0' }
+    },
   },
   plugins: [],
 }
-
